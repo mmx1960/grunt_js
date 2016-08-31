0594f673bd0818931513b4c347e3e865312b72bc
zhangzc <347328331@qq.com>
2016-08-31 21:39:39 +0800

    初次提交

---
+++ Gruntfile.js
@@ -0,0 +1,15 @@
+module.exports = function(grunt){
+	grunt.initConfig({
+		jshint: ['Gruntfile.js'],
+		less:{
+			compile:{
+				files:{
+					'build/css/compiled.css':'public/css/**/*.less'
+					}
+				}
+			}
+	});
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.registerTask('default',['jshint']);
+};
